f8093ac24908f26b49a363ae98f85d9b6d26faa6
jaeyeon90 <dlwodbd@naver.com>
2023-11-12 16:32:29 +0900

    newcommit

---
+++ src/halo2_java.java
@@ -0,0 +1,9 @@
+
+public class halo2_java {
+
+	public static void main(String[] args) {
+	// TODO 	Auto-generated method stub
+		System.out.println("두번재 커밋 및 커밋사항 업데이트 되었는지 확인해주는 문장. 만약 이 글이 제대로 올라왔다면 커밋준비는 완료로 인정 됨!.");
+	}
+
+}
